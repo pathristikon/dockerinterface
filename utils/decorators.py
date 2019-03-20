7ede90e140977c1231609ec2bd77042cea9ed408
Dumitru Alexandru <alexandru.dumitru@webdal.ro>
2019-03-20 10:07:16 +0200

    Initial commit

---
+++ utils/decorators.py
@@ -0,0 +1,57 @@
+import datetime, os
+import subprocess, shlex, sys
+import _thread
+
+def executeCommand(description):
+    """
+    Decorator for executing bash commands
+    :param description:
+    :return:
+    """
+    def wrap(func):
+        def wrapped_f(*args):
+            command = func(*args)
+            text = "\n[NOTICE] " + str(datetime.datetime.now())
+            text += " " + description + "\n"
+            args[0].insert("end", text, "inspect")
+            try:
+                _thread.start_new_thread(run_command, (command, args,))
+            except:
+                print ("Error: unable to start thread")
+        return wrapped_f
+    return wrap
+
+def run_command(command, args):
+    """
+    Execute command in new thread
+    :param command:
+    :param args:
+    :return:
+    """
+    try:
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as err:
+        print("Status : FAIL", err.returncode, err.output)
+
+    else:
+        proc = process.stdout
+
+        while process.poll() is None:
+            args[0].insert("end", proc.readline())
+            args[0].insert("end", "\n")
+            args[0].see("end")
+        sys.stdout.flush()
+
+        if process.returncode != 0:
+            raise Exception("The subprocess did not terminate correctly.")
+
+        # wait for one process to finish
+        # before starting another
+        process.wait()
+
+
+
---
+++ utils/utils.py
@@ -0,0 +1,91 @@
+import os, socket
+from glob import glob
+from . import decorators
+
+BASEDIR = "../"
+
+def getDirs(path):
+    """
+    Get directories from path
+    containing Dockerfile
+    :return:
+    """
+    directories =  next(os.walk(path))[1]
+    alldirs = filter(lambda x: x.startswith(".") == False, directories)
+    for dir in alldirs:
+        if getDockerfiles(dir):
+            yield dir
+
+def checkbaseportisopened(port):
+    """
+    Check if the given port is in use
+    :param port:
+    :return:
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
+
+def getDockerfiles(project):
+    """
+    Get a list of dockerfiles from the project
+    :param project:
+    :return:
+    """
+    return glob(os.path.abspath(BASEDIR + project + "/*ockerfile*"))
+
+def buildDockerfiles(info, dockers, project, all = False):
+    if all:
+        count = 0
+        for d in dockers:
+            count += 1
+            print(d)
+            _build(info, d, project + "_" + str(count))
+    else:
+        _build(info, dockers[0], project)
+
+@decorators.executeCommand("Building Dockerfile(s)")
+def _build(info, path, name):
+    #print("docker build -t " + name + " -f " + path + " " + os.path.dirname(path))
+    return "docker build -t " + name + " -f " + path + " " + os.path.dirname(path)
+
+@decorators.executeCommand("Project changed")
+def getContainers(name):
+    """
+    getting the id of the container
+    :param name:
+    :return:
+    """
+    return "docker ps -aqf name={}".format(name)
+
+def getWarnings():
+    """
+    Get the warnings
+    :return:
+    """
+    ports = (80, 443)
+    for p in ports:
+        if checkbaseportisopened(p):
+           yield "Warning: Port {} already in use \n".format(p)
+
+@decorators.executeCommand("Executing system prune")
+def sysPrune(info):
+    """
+    Execute docker system prune
+    :return:
+    """
+    return "docker system prune -f"
+
+@decorators.executeCommand("Inspecting the current project")
+def inspectProject(info, project):
+    """
+    Inspect the project
+    :param project:
+    :return:
+    """
+    return "docker ps -f name=" + \
+           project + " --format ID={{.ID}}\\nImage={{.Image}}\\nName={{.Names}}\\nPort={{.Ports}}\\n"
+
+def checkIfComposerExists(dir):
+    return "Project has docker-compose.yml\n" \
+        if glob(BASEDIR + dir + '/docker-compose.yml') \
+        else "Project doesn\'t have docker-compose.yml\n"
